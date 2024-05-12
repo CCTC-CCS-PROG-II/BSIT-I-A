81048021d3b68cead8e501569c5680cea433e69a
Christian09232004 <158007121+Christian09232004@users.noreply.github.com>
2024-05-11 23:49:04 -0600

    Update BUHIA_ACTIVITY_V.java

--- Activities/Activity V/BUHIA_ACTIVITY_V.java
+++ Activities/Activity V/BUHIA_ACTIVITY_V.java
@@ -1,20 +1,21 @@
 import java.util.Scanner;
     public class BUHIA_ACTIVITY_V{
     public static void main(String []args) {
-        Scanner keyboard= new Scanner(System.in);
-        
-        System.out.println("Enter your age: ");
-        Int Age = sc.nextInt();
+        Scanner sc = new Scanner(System.in);
+        int age;
 
-        if(age >= 0 && age <= 11){
+        System.out.println("Enter your age: ");
+        age = sc.nextInt();
+
+        if(age>=0 && age<=11){
             System.out.println("Child");
         }
-       
-        else if(age <=12 && age <=17){
+    
+        else if(age<=12 &&  age<=17){
             System.out.println("Teen");
         }
         
-        else if(age >= 18 && age <=64){
+        else if(age>= 18 &&  age<=64){
             System.out.println("Adult");
 
         }
