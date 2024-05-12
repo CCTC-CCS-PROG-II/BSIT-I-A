bf24acf66352e96c2f5a0b69cb1e22116d8b6c95
Christian09232004 <158007121+Christian09232004@users.noreply.github.com>
2024-05-11 23:56:28 -0600

    Update BUHIA_ACTIVITY-1.java

--- Activities (Midterm)/Activity I/BUHIA_ACTIVITY-1.java
+++ Activities (Midterm)/Activity I/BUHIA_ACTIVITY-1.java
@@ -1,4 +1,4 @@
-import java_util.Scanner
+import java.util.Scanner;
 public class BUHIA_ACTIVITY_l {
    public static void main(String[] args) {
         Scanner sc=new Scanner(System.in); 
@@ -6,9 +6,9 @@ public class BUHIA_ACTIVITY_l {
         String [] names = new String[5];
 
         for (int i = 0; i < names.length; i++) {
-        System.out.print("Enter name" + (i + 1) + ": ");
+        System.out.print("Enter name " + (i + 1) + ": ");
         names[i] = sc.nextLine();
-}
+        }
 
         System.out.print("Enter the number on the list of names you want to display: ");
         int index = sc.nextInt();
@@ -17,6 +17,6 @@ public class BUHIA_ACTIVITY_l {
         System.out.println("You have selected " + names[index - 1] + ".");
         } else {
         System.out.println("invalid index");
-}
-}
+        }
+    }
 }
