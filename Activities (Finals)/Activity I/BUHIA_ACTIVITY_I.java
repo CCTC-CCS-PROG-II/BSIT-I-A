8cfbf2542c13caab826cfc8512e19d08a32891d9
Christian09232004 <158007121+Christian09232004@users.noreply.github.com>
2024-05-12 00:21:52 -0600

    Update BUHIA_ACTIVITY_I.java

--- Activities (Finals)/Activity I/BUHIA_ACTIVITY_I.java
+++ Activities (Finals)/Activity I/BUHIA_ACTIVITY_I.java
@@ -1,10 +1,19 @@
 import java.util.Scanner;
 public class BUHIA_ACTIVITY_I{
-        public static void main(String[] args) {
+        public static void main(String[] args){
+            int length = length(); 
+            int width = width();
+            int area = length * width;
+            System.out.println("Area = " + area);
+        }    
+        public static int length(){
             Scanner sc = new Scanner(System.in);
-
-            int length = 20; width = 40;
-            System.out.println("Average = " + length + width/2);
-            System.out.println("Area = " + length * width);
-     }
+            System.out.println("Enter the length of the rectangle: ");
+            return sc.nextInt();
+        }
+        public static int width(){
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter the width of the rectangle: ");
+            return sc.nextInt();
+        }
 }
