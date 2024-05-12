54dac37a0af082766dc9b0304e8bcd0b244f5141
Christian09232004 <158007121+Christian09232004@users.noreply.github.com>
2024-05-12 00:26:11 -0600

    Update BUHIA_ACTIVITY_II.java

--- Activities (Finals)/Activity II/BUHIA_ACTIVITY_II.java
+++ Activities (Finals)/Activity II/BUHIA_ACTIVITY_II.java
@@ -1,14 +1,13 @@
 import java.util.Scanner;
 public class BUHIA_ACTIVITY_II{
-    static void feettoinches(double feet){
-        double res = feet * 12;
+    public static void feettoinches(int feet){
+        int res = feet * 12;
         System.out.println(res);
     }
         public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter your feet: ");
-        double feet = sc.nextDouble();
+        int feet = sc.nextInt();
         feettoinches(feet);
-
-        }
     }
+}
