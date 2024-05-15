e2601633dcfedd8200da7f448fe0388ee0bb5f8f
Christian09232004 <158007121+Christian09232004@users.noreply.github.com>
2024-05-15 09:19:49 -0600

    Update BUHIA_ACTIVITY_IV.java

--- Activities/Activity IV/BUHIA_ACTIVITY_IV.java
+++ Activities/Activity IV/BUHIA_ACTIVITY_IV.java
@@ -3,7 +3,7 @@ public class BUHIA_ACTIVITY_IV {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Red/Green/Black: ");
+        System.out.println("Enter your Color: ");
         String robot = sc.nextLine();
 
         switch(robot){
@@ -16,6 +16,8 @@ public class BUHIA_ACTIVITY_IV {
             case "black":
                 System.out.println("3");
                 break;
+            default:
+                System.out.print("nothing in particular");
         }
     }
 }
