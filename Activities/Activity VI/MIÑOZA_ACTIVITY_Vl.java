2a604e92c3392a89efd83e9670a7ffbd9e6201a3
MAYLENE M. MINOZA <156988633+Maylene99@users.noreply.github.com>
2024-05-18 15:17:47 +0800

    MIÑOZA_ACTIVITY_Vl.java

--- Activities/Activity VI/MIÑOZA_ACTIVITY_Vl.java
+++ Activities/Activity VI/MIÑOZA_ACTIVITY_Vl.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class CLIMACO_ACTIVITY_VI{
+public class MINOZA_ACTIVITY_VI{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter  the number ofseconds ");
